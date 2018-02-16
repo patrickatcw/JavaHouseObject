273ef7d28735ef8c22e3eb16d620363f9af08881
patrickatcw <pj8392@yahoo.com>
2018-02-16 11:55:29 -0500

    step 1 create fields for House

--- src/com/me/House.java
+++ src/com/me/House.java
@@ -1,4 +1,15 @@
 package com.me;
 
 public class House {
+
+    //Step 1 create fields for House
+    private String color;
+    private String style;
+    private String size;
+    private int bedrooms;
+    private int bathrooms;
+    private int year;
+    private String garage;
+    private String basement;
+
 }
